08591d3897cd1cedcb475dd8b5c927b0c2e7f3c8
Anna Clara Luna <annaclara.luna06@gmail.com>
2025-11-05 15:25:05 -0300

    Fiz o CSS da página individual dos jogos

--- cursos.js
+++ cursos.js
@@ -22,6 +22,7 @@
     const descricaoCurso = document.getElementById("curso-descricao");
     const instrucoesCurso = document.getElementById("curso-instrucoes");
     const container = document.getElementById("curso-container");
+    const btnJogo = document.getElementById("btn-jogar");
 
     if (tituloCurso) tituloCurso.textContent = curso.titulo || "";
     if (nomePersonagem) nomePersonagem.textContent = curso["personagem-nome"] || "";
@@ -35,11 +36,16 @@
     console.log("Cor de fundo:", curso["cor-fundo"]);
     console.log("Cor da borda:", curso["cor-borda"]);
 
-    if (container && curso["cor-fundo"]) {
-      container.style.backgroundColor = curso["cor-fundo"];
-    }
-    if (container && curso["cor-borda"]) {
-      container.style.border = `4px solid ${curso["cor-borda"]}`;
+    if (container){
+      if(curso["cor-fundo"]){
+        container.style.backgroundColor = curso["cor-fundo"];
+      }
+      if(curso["cor-borda"]){
+        container.style.border = `4px solid ${curso["cor-borda"]}`;
+        if (btnJogo){
+          btnJogo.style.backgroundColor = curso["cor-borda"];
+        }
+      }
     }
 
   }
