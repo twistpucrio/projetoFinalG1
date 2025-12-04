ef392801361409963a3a1d11ad8f05bc59f3eb82
Anna Clara Luna <annaclara.luna06@gmail.com>
2025-12-04 17:53:31 -0300

    fiz o css do jogo de programação

--- controller/controle-jogoFP_1.js
+++ controller/controle-jogoFP_1.js
@@ -3,3 +3,18 @@ carregar.addEventListener('click', () => {
     const chave = document.getElementById('escolha').value;
     window.location.href = `jogoFP_2.html?situacao=${encodeURIComponent(chave)}`;
 });
+
+const select = document.getElementById("escolha");
+const img = document.getElementById("img-prato");
+
+select.addEventListener("change", () => {
+  const valor = select.value;
+
+  if (valor === "sanduiche") {
+    img.src = "../img/lara_sanduiche.png";
+  } else if (valor === "nada"){
+    img.src = "../img/lara_feliz.png";
+  } else if (valor === "omelete") {
+    img.src = "../img/lara_omelete.png";
+  }
+});
