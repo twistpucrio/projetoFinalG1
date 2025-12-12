4d11d3b0ee6e09714420c7c89e588dd946cb973d
Anna3206 <annaclara.luna06@gmail.com>
2025-12-12 13:45:04 -0300

    mudanças no css

--- controller/controle-pagJogo.js
+++ controller/controle-pagJogo.js
@@ -72,6 +72,7 @@ const btnQuizz = document.getElementById('btn-quizz');
 const urlParams = new URLSearchParams(window.location.search);
 const personagem = urlParams.get('personagem'); 
 const img = document.getElementById("personagem-fala");
+const interrogacao = document.getElementById("interrog");
 
 // Mapeia o nome do personagem para o ID do curso (para o Quizz)
 const mapaPersonagemCurso = {
@@ -92,22 +93,57 @@ const tit = document.getElementById("titulo-escolha");
 const data = await carregarDados(DATA_URL);
 const lista = Array.isArray(data) ? data : (data.cursos || []);
 const curso = lista.find(c => c["personagem-nome"] === personagem);
+
+const modalCSS = document.getElementById("modal-content");
+
+const conteudoModal = document.getElementById("conteudo-modal");
+if (conteudoModal) conteudoModal.textContent = curso["instrucoes"] || "";
+
 if(tit && curso["cor-fundo"]){
   tit.style.color = curso["cor-borda"];
+  modalCSS.style.backgroundColor = curso["cor-borda"];
 
   if (personagem === "Lara") {
     img.src = "../img/lara_fala.png";
+    interrogacao.src = "../img/interrog_fp.png"
   } else if (personagem === "Ayla"){
     img.src = "../img/ayla_fala.png";
+    interrogacao.src = "../img/interrog_ia.png"
   } else if (personagem === "Nia") {
     img.src = "../img/nia_fala.png";
+    interrogacao.src = "../img/interrog_sc.png"
   } else if (personagem === "Sofia"){
     img.src = "../img/sofia_fala.png";
+    interrogacao.src = "../img/interrog_cd.png"
   } else if (personagem === "Kaori"){
     img.src = "../img/kaori_fala.png";
+    interrogacao.src = "../img/interrog_hd.png"
   } else if (personagem === "Maya"){
     img.src = "../img/maya_fala.png";
+    interrogacao.src = "../img/interrog_wd.png"
   }
+
+  const modal = document.getElementById("modal-instrucoes");
+  const btn = document.getElementById("btn-instrucoes");
+  const span = document.querySelector(".close");
+
+  // Abrir modal
+  btn.onclick = () => {
+      modal.style.display = "flex";
+  };
+
+  // Fechar modal clicando no X
+  span.onclick = () => {
+      modal.style.display = "none";
+  };
+
+  // Fechar modal clicando fora da caixa
+  window.onclick = (event) => {
+      if (event.target === modal) {
+          modal.style.display = "none";
+      }
+  };
+
 }
 
 if (btnQuizz && cursoId) {
@@ -153,5 +189,12 @@ if (btnVoltar && curso) {
     if (btnVoltar) {
       btnVoltar.style.backgroundColor = curso["cor-borda"];
     }
+  }
+}
+
+const informacoes = document.getElementById("info");
+if (informacoes && curso) {
+  if (curso["cor-borda"]) {
+    informacoes.style.backgroundColor = curso["cor-borda"];
   }
 }
